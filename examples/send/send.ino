60b920a3e6dfd61c7585decfdcb75238a631ebc7
Nicolas Kessler <nicolaskessler@me.com>
2019-03-26 19:05:34 +0100

    sending works

--- examples/send/send.ino
+++ examples/send/send.ino
@@ -3,20 +3,20 @@
 #include <SPI.h>
 
 static const int spiClk = 1000000; // 1 MHz
-SPIClass vspi(VSPI);
+SPIClass hspi(HSPI);
 
-MCP_CAN CAN0(5, vspi);                           // Set CS to pin 5
+MCP_CAN CAN0(15, &hspi);                           // Set CS to pin 15
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.print("Initializing SPI...");
-  vspi.begin();
+  Serial.println("Initializing SPI...");
+  hspi.begin();
   ///hspi.beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
   // init can bus, baudrate: 500k
-  Serial.print("Initializing CAN...");
-  if(CAN0.begin(CAN_500KBPS) == CAN_OK) Serial.print("can init ok!!\r\n");
-  else Serial.print("Can init fail!!\r\n");
+  Serial.println("Initializing CAN...");
+  if(CAN0.begin(CAN_500KBPS) == CAN_OK) Serial.println("can init ok!!\r\n");
+  else Serial.println("Can init fail!!\r\n");
   pinMode(4, INPUT);
 }
 
@@ -24,6 +24,7 @@ unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
 void loop()
 {
   // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
+  Serial.println("Sending...");
   CAN0.sendMsgBuf(0x00, 0, 8, stmp);  
   delay(1000);                       // send data per 100ms
 }
